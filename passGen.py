4e966b0ea4cd552d0bc582bf83479f340ee47565
Fernando Albar <fernandoalbar@gmail.com>
2021-06-16 22:28:40 +0200

    In main

--- passGen.py
+++ passGen.py
@@ -1,18 +1,19 @@
 import string  
 import random
 
-s1=string.ascii_lowercase
-s2=string.ascii_uppercase
-s3=string.digits
-s4=string.punctuation
+if __name__ == "__main__":
+    s1=string.ascii_lowercase
+    s2=string.ascii_uppercase
+    s3=string.digits
+    s4=string.punctuation
 
-pass_len=int(input("Enter password length: "))
+    pass_len=int(input("Enter password length: "))
 
-s = []
-s.extend(list(s1))
-s.extend(list(s2))
-s.extend(list(s3))
-s.extend(list(s4))
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
 
-print ("Your password: ", end="")
-print ("".join(random.sample(s,pass_len)))
+    print ("Your password: ", end="")
+    print ("".join(random.sample(s,pass_len)))
